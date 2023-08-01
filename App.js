6782455fd2b783ed34fdfa81a4a3bf52f8adb470
Tadidivyaa <126139531+Tadidivyaa@users.noreply.github.com>
2023-08-01 15:16:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import ClickCounter from './ClickCounter';
+import HoverCount from './HoverCount';
+import WithCount from './WithCount';
+function App() {
+  return (
+    <div className="App">
+      <ClickCounter/>
+      <HoverCount/>
+      <WithCount/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ WithCount.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+const UpdatedComponent = OriginalComponent =>{
+class NewComponent extends Component {
+    constructor(){
+        super()
+        this.state={
+            count:0
+        }
+
+        }
+    incrementCount=()=>{
+        this.setState(prevState =>{
+        return{count:prevState.count+1}
+        })
+    }
+
+  render() {
+    return <OriginalComponent count={this.state.count} incrementCount={this.incrementCount}/>
+} 
+
+}
+return NewComponent
+}
+export default  UpdatedComponent
